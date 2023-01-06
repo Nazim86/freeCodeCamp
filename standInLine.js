fce0873df5ca980c8bdd3e4c07c084d7b63aa60f
Nazim86 <43852326+Nazim86@users.noreply.github.com>
2023-01-06 12:35:38 +0400

    standInLine

---
+++ standInLine.js
@@ -0,0 +1,16 @@
+function nextInLine(arr, item) {
+   arr.push(item);
+  let element = arr.shift()
+
+  return element;
+  
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
