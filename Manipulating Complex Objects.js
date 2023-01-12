b98d4e436c95161e34d1895a5ddb46fc1cc2beab
Nazim86 <43852326+Nazim86@users.noreply.github.com>
2023-01-12 22:07:56 +0400

    Create Manipulating Complex Objects.js

---
+++ Manipulating Complex Objects.js
@@ -0,0 +1,19 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+myMusic.push({"artist":"Michael Jackson",
+"title":"pop", "release_year": 1990, "formats":[
+  "cd", "9t", "lp"
+]
+})
